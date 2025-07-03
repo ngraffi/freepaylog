064d996d52fd57d67296fd45c4697d0ebb14d404
ngraffi <43457485+ngraffi@users.noreply.github.com>
2025-07-03 13:36:10 +0900

    Update upload_log.py

--- upload_log.py
+++ upload_log.py
@@ -4,7 +4,6 @@ import webbrowser
 st.markdown("# FREEPAY LOG UPLOAD")
 
 if st.button("로그 전송") :
-#  st.html("<a href=""intent://freepaylink?trantype=upload_log&catid=1111111111#Intent;scheme=smartroapp;package=com.smartro.secapps.freepay>테스트</a>")
     st.html('<a href="smartroapp://freepaylink?trantype=upload_log&catid=1111111111">테스트</a>')
 
 if st.button("웹 브라우저") :
