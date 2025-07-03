b95097accc2fd31e5a6057f37252426e99288ee2
ngraffi <43457485+ngraffi@users.noreply.github.com>
2025-07-03 16:01:44 +0900

    Update upload_log.py

--- upload_log.py
+++ upload_log.py
@@ -3,7 +3,7 @@ import streamlit as st
 # from plyer.facades import Intent
 # from plyer import notification
 
-st.title("FREEPAY 로그 전송")
+st.subheader("FREEPAY 로그")
 
 # if st.button("로그 전송") :
     # my_intent = Intent(
