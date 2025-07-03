a2662b382443988cdef82f86b1132e779f9afa5f
ngraffi <43457485+ngraffi@users.noreply.github.com>
2025-07-03 13:34:33 +0900

    Update upload_log.py

--- upload_log.py
+++ upload_log.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import webbrowser
 
-st.markdown("# FREEPAY LOG UPLOAD")
+st.markdown("# FREEPAY LOG UPLOAD??")
 
 if st.button("로그 전송") :
  webbrowser.open("smartro://freepaylink?trantype=upload_log&catid=1111111111")
