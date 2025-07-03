7b524a826c364018431722e95a2893d1be4cf722
ngraffi <43457485+ngraffi@users.noreply.github.com>
2025-07-03 13:30:31 +0900

    Update upload_log.py

--- upload_log.py
+++ upload_log.py
@@ -5,3 +5,6 @@ st.markdown("# FREEPAY LOG UPLOAD")
 
 if st.button("로그 전송") :
  webbrowser.open("smartro://freepaylink?trantype=upload_log&catid=1111111111")
+
+if st.button("웹 브라우저") :
+ webbrowser.open("https://www.naver.com")
