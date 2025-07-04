80f21913d8a10d30b1968f6f60187042efe8ca5b
ngraffi <43457485+ngraffi@users.noreply.github.com>
2025-07-04 16:51:46 +0900

    Update upload_log.py
    when input length is less than 10, prevent to use the button

--- upload_log.py
+++ upload_log.py
@@ -1,13 +1,19 @@
 import streamlit as st
+from st_keyup import st_keyup
 
 st.set_page_config(
     page_title = "Smartro",
     page_icon = "🚧"
 )
 
-st.subheader("FREEPAY 로그!")
+st.subheader("FREEPAY 로그")
 
-catID = st.text_input("단말기번호", max_chars=10, icon="💳")
-    
-st.link_button(url="smartroapp://freepaylink?trantype=upload_log&catid=" + catID, label="실행하기", icon='🐶', disabled=len(catID)!=10)
+catID = st_keyup("단말기번호", max_chars=10)
+
+st.link_button(
+    url="smartroapp://freepaylink?trantype=upload_log&catid=" + catID, 
+    label="실행하기", 
+    icon='🐶', 
+    disabled=len(catID)!=10, 
+)
 
